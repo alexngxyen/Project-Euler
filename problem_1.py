20ea59699b74fdb0bf6a9cc057df7ec1e782aeb5
alexngxyen <alexaan2@uci.edu>
2022-08-20 16:42:38 -0700

    problem 1 solved

--- problem_1.py
+++ problem_1.py
@@ -1,3 +1,8 @@
+import time
+
+# Start Timer
+start = time.time()
+
 # Initalize Parameters
 value        = 1000                                 # Value of Interest
 count        = 1                                    # Number of Iterations
@@ -13,3 +18,7 @@ while (count < value):
     count += 1
 
 print(sum)
+
+# End Timer
+end = time.time()
+print("Elapsed time =", str.format('{0:.15f}',end - start), "seconds")
