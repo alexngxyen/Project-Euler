a338c237cad8b7b015d260f45bc935155c43ddd4
alexngxyen <alexaan2@uci.edu>
2022-08-22 18:27:28 -0700

    problem 7 solved

--- Python Solutions/problem7.py
+++ Python Solutions/problem7.py
@@ -1,16 +1,34 @@
 # Import Packages
 import timeit
 
+# Functions
+def is_prime(n):
+  for i in range(2, n):
+    if n % i == 0:
+      return False
+  return True
+
 # Start Timer
 start = timeit.default_timer()
 
 # Initialize Parameters
-            
+number_of_primes = 10001                                 # Number of Prime Numbers
+prime_numbers    = []                                    # List of Prime Numbers
+count            = 1                                     # Counter
 
+# Determine Prime Numbers
+while len(prime_numbers) < number_of_primes + 1:
+    # Check If Natural Number is Prime
+    if is_prime(count):
+        # Add number to list of prime numbers
+        prime_numbers.append(count)
+    
+    # Update Counter
+    count += 1
 
 # End Timer
 end = timeit.default_timer()
 
 # Print Results
-# print("Difference between the sum of the squares and the square of the sum for", value, " natural numbers =", square_of_sum - sum_of_square)
+print(number_of_primes, "prime number =", prime_numbers[-1])
 print("Elapsed time =", str.format('{0:.15f}',end - start), "seconds")
