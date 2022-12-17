3d5b0ba539c356fb58c09a0aa0efb4ed8ef92ad2
alexngxyen <alexaan2@uci.edu>
2022-12-17 15:17:03 -0800

    change names

---
+++ Python Solutions/problem_18.py
@@ -0,0 +1,25 @@
+#  ============================================================================
+#  Name        : problem18.py
+#  Description : Find the number of letters used to write out the numbers from 
+#                1 to 1000.
+#  Author      : Alex Nguyen
+#  Date        : August 2022
+#  ============================================================================
+
+# Import Packages
+import numbers
+import timeit
+
+# Start Timer
+start = timeit.default_timer()
+
+# Initalize Parameters
+numbers = 1000
+
+
+# End Timer
+end = timeit.default_timer()
+
+# Print Results
+print("\nNumber of letters from 1 to {}".format(numbers))
+print("Elapsed time =", str.format('{0:.15f}',end - start), "seconds\n")      
