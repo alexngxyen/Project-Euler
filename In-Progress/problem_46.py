dfc1e3e4f284bf8377732795828f020855bf7f20
alexngxyen <alexaan2@uci.edu>
2023-01-02 10:08:10 -0800

    folder for code in progress

---
+++ In-Progress/problem_46.py
@@ -0,0 +1,23 @@
+#  ============================================================================
+#  Name        : problem_46.py
+#  Description : Find the smallest odd composite that cannot be written as the
+#                sum of a prime and twice a square.
+#  Author      : Alex Nguyen
+#  Date        : January 2023
+#  ============================================================================
+
+# Import Packages
+import timeit
+
+# Start Timer
+start = timeit.default_timer()
+
+# Initialize Parameters
+
+
+# End Timer
+end = timeit.default_timer()
+
+# Print Results
+# print("\nNumber of distinct terms in the sequence a**b = {} with a_max, b_max = {}, {}".format(len(ab_sequence), a_max, b_max))
+print("Elapsed time =", str.format('{0:.15f}',end - start), "seconds\n")      
